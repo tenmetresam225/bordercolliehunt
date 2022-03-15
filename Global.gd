827a79f09aa31ee651e69bbbaa153780cffb4ed9
Sam-Allen <0321714@schoolsnet.act.edu.au>
2022-03-15 15:23:18 +1100

    High Score and Timer

--- Global.gd
+++ Global.gd
@@ -3,6 +3,8 @@ extends Node
 var bulletInstanceCount = 0 # Keeps track of how many bullet instances are current
 var enemyBulletInstanceCount = 0
 
+var previousScores = []
+
 var scoringInformation = {
 	"currentScore": 0,
 	"currentPlayer": "User",
--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,13 +1,26 @@
 extends Control
 
+export(int) var countdownMax = 10
+var currentTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process(true)
+	currentTimer = countdownMax
+	$HUD/Countdown.text = str(currentTimer)
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		$HUD/CurrentScore.text = str(GlobalVariables.scoringInformation["currentScore"])
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("Game Over")
+	GlobalVariables.previousScores.push_front( GlobalVariables.scoringInformation["currentScore"])
+	print(GlobalVariables.previousScores)
 
 func _process(delta):
 	$HUD/CurrentScore.text = str(GlobalVariables.scoringInformation["currentScore"])
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
